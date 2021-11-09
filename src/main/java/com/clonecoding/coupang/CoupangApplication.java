80bf7d08d8844492ed1528730ce1cb0ee7ed1842
Chanjoo lee <dlckswn14@gmail.com>
2021-11-09 19:22:57 +0900

    프로젝트 생성

---
+++ src/main/java/com/clonecoding/coupang/CoupangApplication.java
@@ -0,0 +1,14 @@
+package com.clonecoding.coupang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupangApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(CoupangApplication.class, args);
+	}
+
+}
