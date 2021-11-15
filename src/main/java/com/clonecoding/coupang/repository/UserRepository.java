b8e7a13faa3c413e1435c4aa58cd671a9aa1b50d
Chanjoo lee <dlckswn14@gmail.com>
2021-11-16 00:31:31 +0900

    repository 메서드 추가

--- src/main/java/com/clonecoding/coupang/repository/UserRepository.java
+++ src/main/java/com/clonecoding/coupang/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.clonecoding.coupang.repository;
 import com.clonecoding.coupang.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
