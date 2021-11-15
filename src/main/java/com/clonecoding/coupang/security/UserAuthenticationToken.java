e7633ece11173e4ba30fa50fae1eb5680b7cd866
Chanjoo lee <dlckswn14@gmail.com>
2021-11-16 00:30:37 +0900

    UserAuthenticationToken 생성

---
+++ src/main/java/com/clonecoding/coupang/security/UserAuthenticationToken.java
@@ -0,0 +1,52 @@
+package com.clonecoding.coupang.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+public class UserAuthenticationToken implements Authentication {
+    private final UserDetails userDetails;
+    private boolean authenticated;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Set.of();
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return userDetails;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return userDetails;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean authenticated) throws IllegalArgumentException {
+        this.authenticated = authenticated;
+    }
+
+    @Override
+    public String getName() {
+        return userDetails.getUsername();
+    }
+}
