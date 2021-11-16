df9f50010b5333f4c10294cf83cb848ad0544450
Chanjoo lee <dlckswn14@gmail.com>
2021-11-16 13:47:41 +0900

    엔티티로 변환, dto로 변환 메소드 추가

--- src/main/java/com/clonecoding/coupang/dto/UserDto.java
+++ src/main/java/com/clonecoding/coupang/dto/UserDto.java
@@ -1,33 +1,19 @@
 package com.clonecoding.coupang.dto;
 
-import com.clonecoding.coupang.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserDto {
     private long id;
     private String name;
     private String email;
-    private String password;
+    private String hashedPassword;
     private String phoneNumber;
-    private boolean advertising_allow;
-
-    public User toEntity(){
-        return User.builder()
-                .email(email)
-                .password(password)
-                .name(name)
-                .phoneNumber(phoneNumber)
-                .build();
-    }
-
+    private boolean advertisingAllow;
 }
--- src/main/java/com/clonecoding/coupang/service/UserService.java
+++ src/main/java/com/clonecoding/coupang/service/UserService.java
@@ -1,25 +1,52 @@
 package com.clonecoding.coupang.service;
 
 import com.clonecoding.coupang.domain.User;
+import com.clonecoding.coupang.dto.UserCreateDto;
 import com.clonecoding.coupang.dto.UserDto;
 import com.clonecoding.coupang.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final EntityManager entityManager;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public Long putUser(UserDto userDto){
-        User user = userDto.toEntity();
+    public UserDto create(UserCreateDto dto){
+        User user = toEntity(dto);
         userRepository.save(user);
-        return user.getId();
+        return toDto(user);
     }
 
+    public Optional<UserDto> findByEmail(String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        return user.map(this::toDto);
+    }
+
+    private User toEntity(UserCreateDto dto) {
+        String hashedPassword = passwordEncoder.encode(dto.getPassword());
+
+        return User.builder()
+                .email(dto.getEmail())
+                .password(hashedPassword)
+                .name(dto.getName())
+                .phoneNumber(dto.getPhoneNumber())
+                .build();
+    }
+
+    private UserDto toDto(User user){
+        return UserDto.builder()
+                .email(user.getEmail())
+                .name(user.getName())
+                .hashedPassword(user.getPassword())
+                .phoneNumber(user.getPhoneNumber())
+                .build();
+    }
 }
